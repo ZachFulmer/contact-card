ef164f01c2744eaa6ac50be664fb586cf11ee454
Zach Fulmer <Zachfulmer@earthlink.net>
2022-08-31 14:53:19 -0500

    added indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,8 +9,11 @@ import Dog from '../images/dog.png';
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
+
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
